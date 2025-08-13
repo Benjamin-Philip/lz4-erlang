5667566f94b3e91eb11172b2a2c18e3c2d474076
Benjamin Philip <benjamin.philip495@gmail.com>
2025-08-13 18:57:17 +0530

    Add Block HC Functions (#4)
    This commit adds the Block HC Functions, given by LZ4 as:
    
    - `LZ4_compress_HC` as `lz4_nif:compress_hc/2`
    - `LZ4_compress_HC_destSize` as `lz4_nif:compress_hc_dest_size/3`

--- src/lz4_nif.erl
+++ src/lz4_nif.erl
@@ -9,16 +9,24 @@ Manual](https://github.com/lz4/lz4/blob/cacca37747572717ceb1f156eb9840644205ca4f
 for more details.
 """.
 
--export([compress_default/1, decompress_safe/2]).
--export([compress_bound/1, compress_fast/2, compress_dest_size/2, decompress_safe_partial/2]).
+-export([compress_default/1, compress_hc/2, decompress_safe/2]).
+-export([
+    compress_bound/1,
+    compress_fast/2,
+    compress_dest_size/2,
+    compress_hc_dest_size/3,
+    decompress_safe_partial/2
+]).
 -nifs([
     %% Simple Functions
     compress_default/1,
+    compress_hc/2,
     decompress_safe/2,
     %% Advanced Functions
     compress_bound/1,
     compress_fast/2,
     compress_dest_size/2,
+    compress_hc_dest_size/3,
     decompress_safe_partial/2
 ]).
 -on_load(init/0).
@@ -44,6 +52,38 @@ Raises a `badarg` otherwise:
 """.
 -spec compress_default(Src :: binary()) -> Dst :: binary().
 compress_default(_) ->
+    not_loaded(?LINE).
+
+-doc """
+Compresses a binary with the HC algorithm. Analogous to `LZ4_compress_HC`.
+
+Compresses a binary with the HC algorithm given a binary and an positive
+integral compression level. Raises a `badarg` otherwise:
+
+```
+1> Bin = <<186,157,97,206,111,222,177,60,97,148,186,157,97,206,111,
+   186,157,97,206,111,173,54,194,166,118,222,177,60,97,148,186,157,
+   97,206,111,222,177,60,97,148,222,177,60,97,148,222,177,60,97,148>>.
+2> CompressedBin1 = lz4_nif:compress_hc(Bin, 1).
+3> CompressedBin12 = lz4_nif:compress_hc(Bin, 12).
+4> CompressedBin1 =/= CompressedBin12.
+true
+5> byte_size(CompressedBin1) >= byte_size(CompressedBin12).
+true
+6> lz4_nif:decompress_safe(CompressedBin1, byte_size(Bin)) =:= lz4_nif:decompress_safe(CompressedBin12, byte_size(Bin)).
+true
+7> lz4_nif:compress_hc(<<1, 2, 3>>, -2).
+** exception error: bad argument
+     in function  lz4_nif:compress_hc/2
+        called as lz4_nif:compress_hc(<<1,2,3>>,-2)
+8> lz4_nif:compress_hc(foo, 2).
+** exception error: bad argument
+     in function  lz4_nif:compress_hc/2
+        called as lz4_nif:compress_hc(foo,2)
+```
+""".
+-spec compress_hc(Src :: binary(), CompressionLevel :: pos_integer()) -> Dst :: binary().
+compress_hc(_, _) ->
     not_loaded(?LINE).
 
 -doc """
@@ -163,14 +203,19 @@ Compress as many bytes as possible into a binary of fixed length. Analogous to
 `LZ4_compress_destSize`
 
 Compresses as many bytes from binary `Src` as possible into a binary `Dst` of
-fixed length `TargetDstSize`. Returns a tuple pair of the compressed binary and
-the number of bytes compressed from `Src`:
+fixed length `TargetDstSize`. Re-allocates `Dst` to the final required size.
+Returns a tuple pair of the compressed binary and the number of bytes compressed
+from `Src`:
 
 ```
 1> {Compressed, BytesRead} = lz4_nif:compress_dest_size(<<1, 2, 3, 4>>, 3).
 {<<32,1,2>>,2}
 2> lz4_nif:decompress_safe(Compressed, BytesRead).
 <<1,2>>
+3> {Compressed2, BytesRead2} = lz4_nif:compress_dest_size(<<1, 2, 3, 4>>, 10).
+{<<64,1,2,3,4>>,4}
+4> {Compressed2, BytesRead2} = lz4_nif:compress_dest_size(<<1, 2, 3, 4>>, 15).
+{<<64,1,2,3,4>>,4}
 ```
 
 Raises a `badarg` on non binary and integral values:
@@ -189,6 +234,59 @@ Raises a `badarg` on non binary and integral values:
 -spec compress_dest_size(Src :: binary(), TargetDstSize :: integer()) ->
     {Dst :: binary(), BytesRead :: pos_integer()}.
 compress_dest_size(_, _) ->
+    not_loaded(?LINE).
+
+-doc """
+Compress as many bytes as possible with the HC algorithm into a binary of fixed
+length. Analogous to `LZ4_compress_HC_destSize`.
+
+Compresses as many bytes with the HC algorithm with compression level
+`CompressionLevel` from binary `Src` as possible into a binary `Dst` of fixed
+length `TargetDstSize`. Re-allocates the compressed binary to the final required
+size. Returns a tuple pair of the compressed binary and the number of bytes
+compressed from `Src`:
+
+```
+1> Bin = <<186,157,97,206,111,222,177,60,97,148,186,157,97,206,111,
+   186,157,97,206,111,173,54,194,166,118,222,177,60,97,148,186,157,
+   97,206,111,222,177,60,97,148,222,177,60,97,148,222,177,60,97,148>>.
+2> {CompressedBin1, BytesRead1} = lz4_nif:compress_hc_dest_size(Bin, 64, 1).
+3> {CompressedBin12, BytesRead12} = lz4_nif:compress_hc_dest_size(Bin, 64, 12).
+4> CompressedBin1 =/= CompressedBin12.
+true
+5> BytesRead1 >= BytesRead12
+true
+6> lz4_nif:decompress_safe(CompressedBin1, BytesRead1) =:= binary:part(Bin, 0, BytesRead1).
+true
+7> lz4_nif:decompress_safe(CompressedBin12, BytesRead12) =:= binary:part(Bin, 0, BytesRead12).
+true
+8> lz4_nif:compress_hc_dest_size(Bin, 100, 12) =:= lz4_nif:compress_hc_dest_size(Bin, 200, 12).
+true
+```
+
+Raises a `badarg` on non binary values for `Src` and non integral values for
+`TargetDstSize` and `CompressionLevel`:
+
+```
+1> lz4_nif:compress_hc_dest_size(foo, 1, 2).
+** exception error: bad argument
+     in function  lz4_nif:compress_hc_dest_size/3
+        called as lz4_nif:compress_hc_dest_size(foo,1,2)
+2> lz4_nif:compress_hc_dest_size(<<1, 2, 3>>, foo, 3).
+** exception error: bad argument
+     in function  lz4_nif:compress_hc_dest_size/3
+        called as lz4_nif:compress_hc_dest_size(<<1,2,3>>,foo,3)
+3> lz4_nif:compress_hc_dest_size(<<1, 2, 3>>, 3, foo).
+** exception error: bad argument
+     in function  lz4_nif:compress_hc_dest_size/3
+        called as lz4_nif:compress_hc_dest_size(<<1,2,3>>,3,foo)
+```
+""".
+-spec compress_hc_dest_size(
+    Src :: binary(), TargetDstSize :: integer(), CompressionLevel :: pos_integer()
+) ->
+    {Dst :: binary(), BytesRead :: pos_integer()}.
+compress_hc_dest_size(_, _, _) ->
     not_loaded(?LINE).
 
 -doc """
@@ -230,7 +328,6 @@ Raises a `badarg` if:
      in function  lz4_nif:decompress_safe_partial/2
         called as lz4_nif:decompress_safe_partial(<<48,1,2,3>>,foo)
 ```
-
 """.
 -spec decompress_safe_partial(Src :: binary(), TargetDstSize :: pos_integer()) -> Dst :: binary().
 decompress_safe_partial(_, _) ->
--- test/lz4_nif_SUITE.erl
+++ test/lz4_nif_SUITE.erl
@@ -1,8 +1,14 @@
 -module(lz4_nif_SUITE).
 
 -export([all/0]).
--export([compress_default/1, decompress_safe/1]).
--export([compress_bound/1, compress_fast/1, compress_dest_size/1, decompress_safe_partial/1]).
+-export([compress_default/1, compress_hc/1, decompress_safe/1]).
+-export([
+    compress_bound/1,
+    compress_fast/1,
+    compress_dest_size/1,
+    compress_hc_dest_size/1,
+    decompress_safe_partial/1
+]).
 -export([doc_test/1]).
 
 -include_lib("common_test/include/ct.hrl").
@@ -12,12 +18,14 @@ all() ->
     [
         %% Simple Functions
         compress_default,
+        compress_hc,
         decompress_safe,
 
         %% Advanced Functions
         compress_bound,
         compress_fast,
         compress_dest_size,
+        compress_hc_dest_size,
         decompress_safe_partial,
 
         %% DocTests
@@ -37,6 +45,18 @@ compress_default(_Config) ->
     ?assertEqual(CompressedBin, lz4_nif:compress_default(Bin)),
 
     ?assertError(badarg, lz4_nif:compress_default(0)).
+
+compress_hc(_Config) ->
+    Bin = gen_bin(),
+    CompressedBin = lz4_nif:compress_hc(Bin, 2),
+    ?assertEqual(Bin, lz4_nif:decompress_safe(CompressedBin, byte_size(Bin))),
+
+    LargeBin = gen_large_bin(),
+    ?assertNotEqual(lz4_nif:compress_hc(LargeBin, 1), lz4_nif:compress_hc(LargeBin, 12)),
+
+    ?assertError(badarg, lz4_nif:compress_hc(foo, 2)),
+    ?assertError(badarg, lz4_nif:compress_hc(Bin, foo)),
+    ?assertError(badarg, lz4_nif:compress_hc(Bin, -2)).
 
 decompress_safe(_Config) ->
     Bin = gen_bin(),
@@ -73,10 +93,30 @@ compress_dest_size(_Config) ->
     Bin = gen_bin(),
     {CompressBin, BytesRead} = lz4_nif:compress_dest_size(Bin, 3),
     ?assertEqual(binary:part(Bin, 0, BytesRead), lz4_nif:decompress_safe(CompressBin, BytesRead)),
+    ?assertEqual(lz4_nif:compress_dest_size(Bin, 100), lz4_nif:compress_dest_size(Bin, 200)),
 
     ?assertError(badarg, lz4_nif:compress_dest_size(Bin, -2)),
     ?assertError(badarg, lz4_nif:compress_dest_size(Bin, foo)),
     ?assertError(badarg, lz4_nif:compress_dest_size(foo, 3)).
+
+compress_hc_dest_size(_Config) ->
+    Bin = gen_bin(),
+    {CompressBin, BytesRead} = lz4_nif:compress_hc_dest_size(Bin, 3, 9),
+    ?assertEqual(binary:part(Bin, 0, BytesRead), lz4_nif:decompress_safe(CompressBin, BytesRead)),
+    ?assertEqual(
+        lz4_nif:compress_hc_dest_size(Bin, 100, 12), lz4_nif:compress_hc_dest_size(Bin, 200, 12)
+    ),
+
+    LargeBin = gen_large_bin(),
+    ?assertNotEqual(
+        lz4_nif:compress_hc_dest_size(LargeBin, 1024, 1),
+        lz4_nif:compress_hc_dest_size(LargeBin, 1024, 12)
+    ),
+
+    ?assertError(badarg, lz4_nif:compress_hc_dest_size(Bin, -2, 12)),
+    ?assertError(badarg, lz4_nif:compress_hc_dest_size(Bin, foo, 12)),
+    ?assertError(badarg, lz4_nif:compress_hc_dest_size(Bin, 3, foo)),
+    ?assertError(badarg, lz4_nif:compress_hc_dest_size(foo, 3, 12)).
 
 decompress_safe_partial(_Config) ->
     Bin = gen_bin(),
@@ -103,6 +143,29 @@ doc_test(_Config) ->
 %%%%%%%%%%%%%
 %% Helpers %%
 %%%%%%%%%%%%%
+
+gen_large_bin() ->
+    Bin1 = gen_bin(),
+    Bin2 = gen_bin(),
+    Bin3 = gen_bin(),
+    Bin4 = gen_bin(),
+    Bin5 = gen_bin(),
+    BinList = [rand:uniform(5) || _ <- lists:duplicate(30, 0)],
+    <<
+        case X of
+            1 ->
+                Bin1;
+            2 ->
+                Bin2;
+            3 ->
+                Bin3;
+            4 ->
+                Bin4;
+            5 ->
+                Bin5
+        end
+     || X <- BinList
+    >>.
 
 gen_bin() ->
     gen_bin(16).
